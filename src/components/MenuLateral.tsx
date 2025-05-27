2e95c40b8ce9227ce08174a3beb636bb24200400
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-05-27 14:42:33 -0300

    feat: transição suave

--- src/components/MenuLateral.tsx
+++ src/components/MenuLateral.tsx
@@ -1,10 +1,14 @@
 import { BsArrowLeft } from "react-icons/bs";
+import { useState } from "react";
 
 export default function MenuLateral() {
+    const [open, setOpen] = useState(true);
+
+
     return (
         <div className="flex">
-            <div className="bg-dark-purple h-screen p-5 pt-8 w-72">
-                <BsArrowLeft/>
+            <div className= {`bg-dark-purple h-screen p-5 pt-8 transition-all duration-300 ${open ? "w-72" : "w-20" } relative`}>
+                <BsArrowLeft className="bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-9 border border-dark-purple cursor-pointer" onClick={() => setOpen(!open)}/>
             </div>
             <div className="p-7"><h1 className="text-2xl font-semibold">Página Inicial</h1></div>
 
