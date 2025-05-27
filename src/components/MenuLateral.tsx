f398985d8e6071d2fa55a95ec04a664117a4da0f
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-05-27 14:28:45 -0300

    feat: icon

--- src/components/MenuLateral.tsx
+++ src/components/MenuLateral.tsx
@@ -1,7 +1,11 @@
+import { BsArrowLeft } from "react-icons/bs";
+
 export default function MenuLateral() {
     return (
         <div className="flex">
-            <div className="bg-dark-purple h-screen p-5 pt-8 w-72">Barra Lateral</div>
+            <div className="bg-dark-purple h-screen p-5 pt-8 w-72">
+                <BsArrowLeft/>
+            </div>
             <div className="p-7"><h1 className="text-2xl font-semibold">Página Inicial</h1></div>
 
         </div>
