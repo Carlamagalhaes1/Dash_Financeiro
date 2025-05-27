3fead86e053b9653b422c4d9556a40fd4b1b9b23
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-05-27 14:45:27 -0300

    rotação da seta

--- src/components/MenuLateral.tsx
+++ src/components/MenuLateral.tsx
@@ -8,7 +8,7 @@ export default function MenuLateral() {
     return (
         <div className="flex">
             <div className= {`bg-dark-purple h-screen p-5 pt-8 transition-all duration-300 ${open ? "w-72" : "w-20" } relative`}>
-                <BsArrowLeft className="bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-9 border border-dark-purple cursor-pointer" onClick={() => setOpen(!open)}/>
+                <BsArrowLeft className={`bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-9 border border-dark-purple cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}/>
             </div>
             <div className="p-7"><h1 className="text-2xl font-semibold">Página Inicial</h1></div>
 
