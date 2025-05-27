89fbc431970ffc78f35dd53a9b0516861b01e3f6
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-05-27 14:23:52 -0300

    instalação dos icones

--- src/components/MenuLateral.tsx
+++ src/components/MenuLateral.tsx
@@ -1,8 +1,10 @@
 export default function MenuLateral() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <div className="flex">
+            <div className="bg-dark-purple h-screen p-5 pt-8 w-72">Barra Lateral</div>
+            <div className="p-7"><h1 className="text-2xl font-semibold">Página Inicial</h1></div>
+
+        </div>
     )
 
 }
