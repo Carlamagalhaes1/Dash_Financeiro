875d963b5a19c542999f8fe3bb4292ea7b54f682
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-06-02 15:19:22 -0300

    lupa

--- src/components/MenuLateral.tsx
+++ src/components/MenuLateral.tsx
@@ -1,6 +1,7 @@
-import { BsArrowLeft } from "react-icons/bs";
+import { BsArrowLeft, BsSearch } from "react-icons/bs";
 import { useState } from "react";
 import { AiFillEnvironment } from "react-icons/ai";
+
 
 export default function MenuLateral() {
     const [open, setOpen] = useState(true);
@@ -8,12 +9,19 @@ export default function MenuLateral() {
 
     return (
         <div className="flex">
-            <div className= {`bg-dark-purple h-screen p-5 pt-8 transition-all duration-300 ${open ? "w-72" : "w-20" } relative`}>
-                <BsArrowLeft className={`bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-9 border border-dark-purple cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}/>
-            <div className="inline-flex">
-               <AiFillEnvironment className={`bg-amber-300 text-4xl rounded cursor-pointer block float-left mr-2  duration-300 ${open && 'rotate-[360deg]'}`} />
-               <h1 className={`text-white origin-left font-medium text-2xl  duration-200 ${!open && "scale-0"}    `}>Tailwind</h1> </div>        
+            <div className={`bg-dark-purple h-screen p-5 pt-8 transition-all duration-300 ${open ? "w-72" : "w-20"} relative`}>
+                <BsArrowLeft className={`bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-9 border border-dark-purple cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)} />
+                <div className="inline-flex">
+                    <AiFillEnvironment className={`bg-amber-300 text-4xl rounded cursor-pointer block float-left mr-2  duration-300 ${open && 'rotate-[360deg]'}`} />
+                    <h1 className={`text-white origin-left font-medium text-2xl  duration-200 ${!open && "scale-0"}    `}>Tailwind</h1></div>
+                <div className={`flex items-center rounded-md bg-light-white mt-6 ${!open ? "px-2.5" : "px-4"} py-2`}>
+                    <BsSearch className={`text-white text-lg block float-left cursor-pointer ${open && "mr-2"} `} />
+                    <input type={"search"} placeholder="pesquisar" className={`text-base bg-transparent w-full text-white focus:outline-none ${!open && "hidden"}`}/>
+
+                </div>
+
             </div>
+
             <div className="p-7"><h1 className="text-2xl font-semibold">Página Iniciali</h1></div>
 
         </div>
