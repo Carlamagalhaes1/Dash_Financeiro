795b922fed3f78615c9191cc63bd3ff625d3c301
Carla Magalhaes <carlamagalhaes60@gmail.com>
2025-06-04 22:24:56 -0300

    feat: cor

--- src/components/MenuLateral.tsx
+++ src/components/MenuLateral.tsx
@@ -13,7 +13,7 @@ export default function MenuLateral() {
                 <BsArrowLeft className={`bg-white text-dark-purple text-3xl rounded-full absolute -right-3 top-9 border border-dark-purple cursor-pointer ${!open && "rotate-180"}`} onClick={() => setOpen(!open)} />
                 <div className="inline-flex">
                     <AiFillEnvironment className={`bg-amber-400 text-4xl rounded cursor-pointer block float-left mr-2  duration-300 ${open && 'rotate-[360deg]'}`} />
-                    <h1 className={`text-white origin-left font-medium text-2xl  duration-200 ${!open && "scale-0"}    `}>Tailwind</h1></div>
+                    <h1 className={`text-white origin-left font-medium text-2xl  duration-200 ${!open && "scale-0"}    `}>Tailwind.</h1></div>
                 <div className={`flex items-center rounded-md bg-light-white mt-6 ${!open ? "px-2.5" : "px-4"} py-2`}>
                     <BsSearch className={`text-white text-lg block float-left cursor-pointer ${open && "mr-2"} `} />
                     <input type={"search"} placeholder="pesquisar" className={`text-base bg-transparent w-full text-white focus:outline-none ${!open && "hidden"}`}/>
